7d97ea893a7f7117d3fb070c1e793779f90c8135
Ivan Zotov <zia14leg@gmail.com>
2023-02-20 22:06:59 +0400

    feat: make contact form adaptive

--- components/GetInTouch.tsx
+++ components/GetInTouch.tsx
@@ -5,7 +5,8 @@ import ContactForm from './ContactForm';
 export default function GetInTouch(): JSX.Element {
   return (
     <section id='get-in-touch' className={styles.getInTouch}>
-      <div className={styles.border}>
+      <div className={styles.wrapper}>
+        <div className={styles.border}></div>
         <h2 className={styles.heading}>Contact us</h2>
         <p className={`${styles.description} p1`}>Do you have any kind of help please contact with us.</p>
         <ContactForm />
